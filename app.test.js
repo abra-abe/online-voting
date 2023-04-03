24e4b463f8b78f2c3772ea8ef4c302ce84c23b50
abra-abe <abewills17@gmail.com>
2023-04-03 04:03:09 +0300

    added .circleci/config.yml file

--- app.test.js
+++ app.test.js
@@ -2,6 +2,18 @@ const app = require("./app");
 const request = require("supertest");
 
 describe("POST /test", () => {
+    describe("when the home page is reached", () => {
+        test("Should respond with status code 200", async () => {
+            const response = await request(app).get("/")
+            expect(response.status).toBe(200)
+        })
+    })
+    describe("Is the database connected successfully to the node js app", () => {
+        test("the api endpoint /results should respond with status code 200", async () => {
+            const response = await request(app).get("/results")
+            expect(response.status).toBe(200)
+        })
+    })
     describe("When username an password are passed", () => {
         test("should be redirected and respond with status 302", async () => {
             const response = await request(app).post("/register").send({
