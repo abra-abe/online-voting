59d95ec2c2586ac66c920b3df59eed26d1bd8b44
abra-abe <abewills17@gmail.com>
2023-04-04 06:05:04 +0300

    fixed tests

--- app.test.js
+++ app.test.js
@@ -2,6 +2,18 @@ const app = require("./app");
 const request = require("supertest");
 
 describe("POST /test", () => {
+    describe("When username an password are passed", () => {
+        test("should be redirected and respond with status 302", async () => {
+            const response = await request(app).post("/register").send({
+                username: "username", 
+                password: "password"
+            })
+            expect(response.status).toBe(302)
+        })
+    })
+})
+
+describe("GET /test", () => {
     describe("when the home page is reached", () => {
         test("Should respond with status code 200", async () => {
             const response = await request(app).get("/")
@@ -12,15 +24,6 @@ describe("POST /test", () => {
         test("the api endpoint /results should respond with status code 200", async () => {
             const response = await request(app).get("/results")
             expect(response.status).toBe(200)
-        })
-    })
-    describe("When username an password are passed", () => {
-        test("should be redirected and respond with status 302", async () => {
-            const response = await request(app).post("/register").send({
-                username: "username", 
-                password: "password"
-            })
-            expect(response.status).toBe(302)
         })
     })
 })
